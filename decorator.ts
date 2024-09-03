55f95144f883f0ce5b6a62cf67de6f6459c25def
Tafita ANDONIAINA <tafitatsanta@gmail.com>
2024-09-03 10:09:30 +0300

    [FEAT] creation of components

---
+++ decorator.ts
@@ -0,0 +1,13 @@
+class ModelS {
+    public description = "Model S";
+    public cost(): number {
+        return 100000;
+    }
+}
+
+class ModelX {
+    public description = "Model X";
+    public cost(): number {
+        return 300000;
+    }
+}
