694e6c5fc519a5ad2ad2081dd6c9d3c5d9f43af7
Tafita ANDONIAINA <tafitatsanta@gmail.com>
2024-09-03 10:23:08 +0300

    [FEAT] decorators

--- decorator.ts
+++ decorator.ts
@@ -1,3 +1,12 @@
+abstract class Car {
+    public description: string;
+    public getDescription(): string {
+        return this.description;
+    }
+
+    public abstract cost(): number;
+}
+
 class ModelS {
     public description = "Model S";
     public cost(): number {
@@ -11,3 +20,46 @@ class ModelX {
         return 300000;
     }
 }
+
+abstract class CarOptions extends Car {
+    decoratedCar: Car;
+    public abstract getDescription(): string;
+    public abstract cost(): number;
+}
+
+class EnhancedAutoPilot extends CarOptions {
+    decoratedCar: Car;
+
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + ', Enhanced AutoPilot';
+    }
+    public cost(): number {
+        return this.decoratedCar.cost() + 10000;
+    }
+
+}
+
+
+class RearFacingSeats extends CarOptions {
+    decoratedCar: Car;
+
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + ', Rear facing seats';
+    }
+    public cost(): number {
+        return this.decoratedCar.cost() + 5000;
+    }
+
+}
+
+
