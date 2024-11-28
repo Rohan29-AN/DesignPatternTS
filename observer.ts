d82e5c7c065e9a4ca6ef04dd90082d6c8dce80e2
Rohan <rohanrc101@gmail.com>
2024-11-28 11:40:14 +0300

    [FEAT] implementing subject interface methods

--- observer.ts
+++ observer.ts
@@ -9,10 +9,29 @@ interface Observer{
 }
 
 
-class weatherStation {
+class weatherStation implements Subject{
+   
     private temperature:number;
+    private observers:Observer[]=[];
 
     setTemperature(temp:number){
+        console.log("Weather station : new temperature measurement: "+temp);
         this.temperature=temp;
+        this.notifyObservers();
+    }
+
+    registerObserver(obs: Observer) {
+        this.observers.push(obs);
+    }
+    removeObserver(obs: Observer) {
+        const index=this.observers.indexOf(obs);
+        if(index!== -1){
+            this.observers.slice(index,1);
+        }
+    }
+    notifyObservers() {
+        for(let i=0;i<this.observers.length;i++){
+            this.observers[i].update(this.temperature);
+        }
     }
 }
