548fe697303802c25e72f1fca0c6616685a3efcf
Rohan <rohanrc101@gmail.com>
2024-11-29 11:20:14 +0300

    [FEAT] creating objects

---
+++ facade.ts
@@ -0,0 +1,34 @@
+class Amplifier{
+    on(){
+        console.log("Applifier is turning on...");
+    }
+    off(){
+        console.log("Amplifier is turning off...")
+    }
+}
+
+
+class Lights{
+    dim(){
+        console.log("Lights are dimming...")
+    }
+}
+
+class Tv{
+    on(){
+        console.log("TV is turning on...");
+    }
+
+    off(){
+        console.log("TV is turning");
+    }
+}
+
+class PopcornMaker{
+    on(){
+        console.log("Popcorn maker is turning on...");
+    }
+    off(){
+        console.log("Popcorn maker is turning off...");
+    }
+}
