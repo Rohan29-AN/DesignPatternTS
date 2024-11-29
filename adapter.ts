12982a4dbe2a0d47103b0b54c2c54b1b002d8182
Rohan <rohanrc101@gmail.com>
2024-11-29 12:05:17 +0300

    [FEAT] implementing adapters

--- adapter.ts
+++ adapter.ts
@@ -1,8 +1,47 @@
-interface Iphone{
+interface Iphone {
     useLighting();
 }
 
-interface Android{
+interface Android {
     useMicroUsb();
 }
 
+class iphoneDevice implements Iphone {
+    useLighting() {
+        console.log('Using lightning port ...');
+    }
+}
+
+class androidDevice implements Android {
+    useMicroUsb() {
+        console.log('Using micro usb ...');
+    }
+}
+
+class LightingToMicroUsbAdapter implements Android {
+    private iphone: iphoneDevice;
+
+    constructor(device: iphoneDevice) {
+        this.iphone = device;
+
+    }
+    useMicroUsb() {
+        console.log("Want to use micro USB , converting to Lighting...");
+        this.iphone.useLighting()
+    }
+}
+
+class MicroUsbToLightningAdapter implements Iphone{
+    private android: androidDevice;
+
+    constructor(device: androidDevice) {
+        this.android = device;
+
+    }
+
+    useLighting() {
+        console.log("Want to use lightning , converting to micro usb...");
+        this.android.useMicroUsb()
+    }
+
+}
